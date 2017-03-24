5dcd37642025855111b7e60a6056f6daf9adc0d4
Mike-415 <mgonz160@mail.ccsf.edu>
2017-03-24 12:57:51 -0700

    -Centered the map -Added colors to map markers -onClickListener for info window

--- app/src/main/java/goldrushtrail/org/goldrushtrail/MapsActivity.java
+++ app/src/main/java/goldrushtrail/org/goldrushtrail/MapsActivity.java
@@ -1,20 +1,36 @@
 package goldrushtrail.org.goldrushtrail;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
+    private TextView infoWindowTitle;
+    private ImageView infoWindowImage;
 
     private GoogleMap mMap;
     private ArrayList<GoldCoastLocation> mLocations;
@@ -41,7 +57,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -54,13 +69,99 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
+
         mMap = googleMap;
+
+
+        for(int i= 0; i<=5; i++)
 
         for( GoldCoastLocation location: mLocations)
         {
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title(location.getTitle()));
+            //ss.setSpan(new  BackgroundColorSpan(Color.YELLOW ),57  ,68  ,0);
+            //title.setSpan(new BackgroundColorSpan(LABEL_COLORS.get(location.tourEnum())) , 0, location.getTitle().length(), 0);
+            //public static enum TOUR_ENUM {YB, EM, JS, FI, PS, CO};
+
+            //TODO: Create a helper method that determines the marker color based off of the tour type
+            //TODO: Create a method that splits the title string and adds a '\n' in the middle if it exceeds a certain length, since some titles go off the screen.
+            /*
+            markerOptions = new MarkerOptions()
+                .title()
+                .icon(getIcon(location.toursEnum()))
+             */
+            MarkerOptions markerOptions;
+
+            if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.YB)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                        .title(location.getTitle());
+            }
+            else if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.EM)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                        .title(location.getTitle());
+            }
+            else if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.JS)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                        .title(location.getTitle());
+            }
+            else if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.FI)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                        .title(location.getTitle());
+            }
+            else if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.PS)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                        .title(location.getTitle());
+            }
+            else if(location.tourEnum() == GoldCoastLocation.TOUR_ENUM.CO)
+            {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+                        .title(location.getTitle());
+            }else {
+                markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                        .title(location.getTitle());
+            }
+
+
+            Log.d("In the for-loop", "location.tourEnum() : "+location.tourEnum());
+            mMap.addMarker(markerOptions);
+
         }
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.795197,-122.404224), 14));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.795197,-122.400000), 14));
+
+
+        //PolygonOptions polygonOptions = new PolygonOptions().addAll(Iterable<LatLng>);
+        //mMap.addPolygon(polygonOptions);
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener(){
+
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                //TODO: Put the location.getTitle() in an intent for the GCLDetailActivity, so that it can iterate through the ArrayList to find the GCLObject
+                //TODO: Ask Karl if you should implement the DetailActivity now or wait
+                String title = marker.getTitle();
+                //Intent intent = new Intent(this, DetailFragment.class); //TODO: Put the appropriate class file in here
+                //intent.putExtras(title);
+                Toast.makeText(getApplicationContext(), "Info window clicked \n"+title, Toast.LENGTH_SHORT).show();
+                //startActivity(intent);
+            }
+        });
     }
 }
